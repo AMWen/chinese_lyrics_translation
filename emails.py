03a6023414258d11b0fd97aa5bc8561639f05274
AMWen <51128121+AMWen@users.noreply.github.com>
2020-09-07 13:43:12 -0400

    updated with utility functions

---
+++ emails.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Import necessary packages
+from smtplib import SMTP
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from utils import *
+
+
+# Find and shuffle songs
+songs = shuffle()
+
+# Loop until get song with translations or run out of songs
+i = 0
+cont = True
+while cont:
+    # Get song information
+    song_link, title, artist, lyrics, video_link = song_info(songs[i])
+    
+    # Translate song
+    translations = translate(title+artist+lyrics)
+    
+    # Iterate counter and continue loop if no translations and still have songs remaining
+    i += 1
+    cont = (i < len(songs)) and len(translations) == 0
+
+# Make translations into one giant block of text
+translations = '\n'.join(translations)
+
+
+# Set up the SMTP server
+s = SMTP(host='smtp.gmail.com', port=587)
+s.starttls()
+s.login('XYZ@gmail.com', 'pwdstring')
+
+# Set up the parameters of the message
+msg = MIMEMultipart()
+msg['From'] = 'name'
+msg['To'] = 'XYZ@gmail.com'
+msg['Subject'] = 'Daily digest'
+
+# Create message
+message = 'Song of the day: {}\n'.format(title) + \
+    'Artist: {}\n\n'.format(artist) + \
+    'Song link: {}\n'.format(song_link) + \
+    'Video link: {}\n\n'.format(video_link) + \
+    'Lyrics: {}\n\n'.format(lyrics) + \
+    'Translations: \n{}'.format(translations)
+
+# Add in the message body
+msg.attach(MIMEText(message, 'plain'))
+
+# Send the message
+s.send_message(msg)
+del msg
+
+# Terminate the SMTP session and close the connection
+s.quit()
---
+++ utils.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+
+# Import necessary packages
+from chinese import ChineseAnalyzer
+from urllib.request import urlopen, Request
+from urllib.parse import quote
+import random
+import re
+import json
+
+
+# Headers for urllib requests
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+
+
+# Function to get shuffled list of songs
+def shuffle():
+    chart_html = urlopen(Request('https://kma.kkbox.com/charts/weekly/song?cate=297', headers=headers)).read()
+    songs = re.findall(r'"song_url":"https:\\/\\/www.kkbox.com\\/tw\\/tc\\/song\\/(\S{24}-index.html)"',
+                       chart_html.decode())
+
+    # Shuffle the songs
+    random.shuffle(songs)
+
+    return songs
+
+
+# Function to find song title, artist, lyrics, and youtube link
+def song_info(song):
+    # Get the song link
+    song_link = 'https://www.kkbox.com/tw/tc/song/' + song
+    print(song_link)
+    
+    # Find title of song and clean up a bit
+    song_html = urlopen(Request(song_link, headers=headers)).read()
+    title = re.findall(r'\<title\>([^\t\n\r\f\v]*)-歌詞', song_html.decode())[0] \
+        .replace('&lt;', '<').replace('&gt;', '>')
+    print(title)
+
+    # Find artist name
+    artist = re.findall(r'\<title\>[^\t\n\r\f\v]*-歌詞-([^\t\n\r\f\v]*)-', song_html.decode())[0] \
+        .replace('&lt;', '<').replace('&gt;', '>')
+    print(artist)
+    
+    # Find lyrics and clean up a bit
+    lyrics = re.findall(r'"text":([^\t\n\r\f\v}]*)', song_html.decode())[0] \
+        .replace('\\n', '\n').replace('"', '').replace('\\r', '')
+
+    # Get video link from youtube
+    html = urlopen("https://www.youtube.com/results?search_query=%s"%(quote(title + artist))).read()
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.decode())
+    video_link = "https://www.youtube.com/watch?v=" + video_ids[0]
+    print(video_link)
+
+    return song_link, title, artist, lyrics, video_link
+
+
+# Function to get list of words from seen or exceptions list
+def words_io(typ='except', words=None):
+    # Specify file path
+    if typ == 'except':
+        save_file = '/path/to/exceptions/file'
+    else:
+        save_file = '/path/to/seen/words/file'
+
+    # Get list of words if none given
+    if words is None:
+        f = open(save_file, 'r')
+        words = json.load(f)
+        f.close()
+        
+        return words
+
+    # Else write list of words to file
+    else:
+        with open(save_file, 'w') as f:
+            f.write(json.dumps(list(words)))
+
+
+# Function to add list of words to seen or exceptions list
+def add(word_list, typ='except'):
+    # Get list of words
+    words = words_io(typ)
+    
+    # Append new word_list to list
+    words.extend(word_list)
+    
+    # Print new list
+    print(words)
+    
+    # Save new list
+    words_io(typ, words)
+
+
+# Function to remove list of words from seen or exceptions list
+def remove(word_list, typ='except'):
+    # Get list of words
+    words = set(words_io(typ))
+    
+    # Remove new word_list
+    words.difference_update(set(word_list))
+    
+    # Print new list
+    print(words)
+    
+    # Save new list
+    words_io(typ, words)
+
+
+# Function to translate the lyrics
+def translate(lyrics):
+    analyzer = ChineseAnalyzer()
+    result = analyzer.parse(lyrics, traditional=True)
+    
+    # Get list of words seen before
+    seen = set(words_io('seen'))
+    
+    # Get list of words on exception list (want to review again)
+    exceptions = set(words_io())
+    
+    # Remove exceptions from seen list
+    seen.difference_update(set(exceptions))
+    
+    # Create list of unique words not seen before or want to review
+    words = [word for word in result.tokens() if not (word in seen or seen.add(word))]
+    
+    # Save new list of words
+    words_io('seen', seen)
+
+    # Convert word list append pinyin and translation(s) to each word
+    translations = []
+    for word in words:
+        defs = result[word]
+        
+        # Only proceed if matches to a word with a definition and pinyin
+        if defs[0].pinyin != None:
+            
+            # Go through all definitions if multiple (since dictionary need a separate key for each)
+            for d in defs:
+                translations.append(word + ': ' + ''.join(d.pinyin) + ', ' + ', '.join(d.definitions))
+
+    return translations
