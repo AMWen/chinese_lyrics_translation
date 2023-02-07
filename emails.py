0432f8304bcb88cca94203f64f8976f1652bc497
Amy Wen <wen.amy@gmail.com>
2023-02-07 10:20:07 -0500

    update to using google access tokens

--- emails.py
+++ emails.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
 # Import necessary packages
+import base64
+import json
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from smtplib import SMTP
+from urllib.parse import urlencode
+from urllib.request import urlopen
 
 from utils import shuffle, song_info, translate
 
@@ -28,15 +32,41 @@ while cont:
 translations = '\n'.join(translations)
 
 
+# Gmail variables and tokens
+GOOGLE_ACCOUNTS_BASE_URL = 'https://accounts.google.com'
+GOOGLE_CLIENT_ID = 'XXX.apps.googleusercontent.com'  # TODO: update this
+GOOGLE_CLIENT_SECRET = 'XXX'  # TODO: update this
+GOOGLE_REFRESH_TOKEN = 'XXX'  # TODO: update this
+
+# Emails
+username = 'XXX@gmail.com'  # TODO: update this
+toaddr = 'XXX@gmail.com'  # TODO: update this
+
+# Get access token
+params = {
+    'client_id': GOOGLE_CLIENT_ID,
+    'client_secret': GOOGLE_CLIENT_SECRET,
+    'refresh_token': GOOGLE_REFRESH_TOKEN,
+    'grant_type': 'refresh_token',
+}
+request_url = f'{GOOGLE_ACCOUNTS_BASE_URL}/o/oauth2/token'
+response = urlopen(request_url, urlencode(params).encode('UTF-8')).read().decode('UTF-8')
+access_token = json.loads(response)['access_token']
+
+# Get authorization string
+auth_string = f'user={username}\1auth=Bearer {access_token}\1\1'
+auth_string = base64.b64encode(auth_string.encode('ascii')).decode('ascii')
+
 # Set up the SMTP server
 s = SMTP(host='smtp.gmail.com', port=587)
+s.ehlo(GOOGLE_CLIENT_ID)
 s.starttls()
-s.login('XYZ@gmail.com', 'pwdstring')  # TODO: update this
+s.docmd('AUTH', 'XOAUTH2 ' + auth_string)
 
 # Set up the parameters of the message
 msg = MIMEMultipart()
-msg['From'] = 'name'  # TODO: update this
-msg['To'] = 'XYZ@gmail.com'  # TODO: update this
+msg['From'] = username
+msg['To'] = toaddr
 msg['Subject'] = 'Daily digest'
 
 # Create message
